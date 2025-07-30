29a1972464a0555b8c1ebe66a72c579629cd4144
PhilG112 <pgrujovski@protonmail.com>
2025-07-30 13:20:07 +1000

    initn

---
+++ GrpcNet.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using GrpcNet.Client;
+using System.Diagnostics;
+
+using var chan = GrpcChannel.ForAddress("http://localhost:5103");
+
+var client = new Greeter.GreeterClient(chan);
+
+for (var i = 0; i < 10; i++)
+{
+    var startTs = Stopwatch.GetTimestamp();
+    var reply = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+    var elapsed = Stopwatch.GetElapsedTime(startTs);
+    Console.WriteLine($"Elapsed time: {elapsed.TotalMilliseconds} ms");
+}
